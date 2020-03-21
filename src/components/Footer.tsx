031503fea599da6dc406c5784eb1400b9e721d79
Emelie Rosenlöw <rosenlowemelie@gmail.com>
2020-03-21 20:21:25 +0100

    Fixed the first issues with responsiveness, added grid, added footer, added pictures

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,21 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import { Footer as AFooter, Anchor } from "grommet";
+import { Facebook, Instagram, Mail } from "grommet-icons";
 
 interface Props {}
 
 export default class Footer extends Component<Props> {
   constructor(props: Props) {
-    super(props)
+    super(props);
   }
 
   render() {
-  return (
-    <h1>footer hej</h1>
-  )}
+    return (
+      <AFooter background="#d7ccc8" pad="medium" justify="center">
+        <Anchor label={<Facebook />} />
+        <Anchor label={<Instagram />} />
+        <Anchor label={<Mail />} />
+      </AFooter>
+    );
+  }
 }
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,14 +1,49 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import Image from "../assets/ceramics1.jpg";
+import { Grid, Box, Image as Picture } from "grommet";
 
 interface Props {}
 
 export default class MainContent extends Component<Props> {
   constructor(props: Props) {
-    super(props)
+    super(props);
   }
 
   render() {
-  return (
-    <h1>main hej</h1>
-  )}
+    return (
+      <Grid
+        margin="2rem"
+        rows={["auto", "auto"]}
+        columns={["auto", "auto"]}
+        gap="medium"
+        areas={[
+          { name: "box1", start: [0, 0], end: [1, 0] },
+          { name: "box2", start: [1, 0], end: [1, 1] },
+          { name: "box3", start: [0, 1], end: [0, 1] },
+          { name: "box4", start: [1, 1], end: [1, 1] }
+        ]}
+      >
+        <Box gridArea="box1" background="light-2" elevation="medium">
+          <Box height="medium" width="medium">
+            <Picture fit="cover" src={Image} />
+          </Box>
+        </Box>
+        <Box gridArea="box2" background="light-2" elevation="medium">
+          <Box height="medium" width="medium">
+            <Picture fit="cover" src={Image} />
+          </Box>
+        </Box>
+        <Box gridArea="box3" background="light-2" elevation="medium">
+          <Box height="medium" width="medium">
+            <Picture fit="cover" src={Image} />
+          </Box>
+        </Box>
+        <Box gridArea="box4" background="light-2" elevation="medium">
+          <Box height="medium" width="medium">
+            <Picture fit="cover" src={Image} />
+          </Box>
+        </Box>
+      </Grid>
+    );
+  }
 }
