2fe19ddafaff66d93356c9b193e67b5f7b900afd
Elin Alm <elin95alm@gmail.com>
2020-04-02 22:08:28 +0200

    orderbekräftelse

--- src/components/Beställningsbekräftelse.tsx
+++ src/components/Beställningsbekräftelse.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useContext } from "react";
+import {grommet, Text, Heading, Box, Paragraph, Image} from 'grommet';
+import { CartContext } from "../contexts/CartContext";
+import { createWriteStream } from "fs";
+import { ShippingContext, ShippingDataProvider } from "../contexts/ShippingContext";
+
 
 export const Beställningsbekräftelse = () => {
-  return <div>Hej</div>;
-};
+  const { totalCost, cart } = useContext(CartContext);
+  const {setShipping} = useContext(ShippingContext)
+
+const ordernr = (max: number)  => {
+  return (
+    Math.floor(Math.random() * Math.floor(max))
+  )
+}
+  
+  return(
+    <Box align="center" fill={true}>
+      <Heading>Tack för din order!</Heading>
+          <Text>Ditt ordernr är: {ordernr(2839873)} </Text>
+          <Paragraph>
+            Den totala summan på din order blev: {totalCost}
+            {setShipping}
+
+            {/* här vill vi hämta det vi har köpt, vad det kostade, och när det ska levereras */}
+          </Paragraph>
+          <Box fill={true}>
+          {cart.map(item => (
+            <Box
+              width="small"
+              height="small"
+              margin="small"
+              elevation="medium"
+              responsive
+            >
+              <Text>
+                {item.product.title + "   "}
+                {item.product.price + " :- /st"}
+              </Text>
+
+              <Image src={item.product.mainImg} fit="cover"/>
+              <div>
+                <Text>Antal: {item.count}</Text>
+              </div>
+            </Box>
+          ))}
+          
+          </Box>
+          </Box>
+  )}
