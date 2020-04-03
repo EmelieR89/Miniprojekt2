fbd3541f0f3beecef84deee3c59aa6096dfd6bd5
Emelie Rosenlöw <rosenlowemelie@gmail.com>
2020-04-03 10:48:13 +0200

    Updates

---
+++ precache-manifest.563244fffa3fac175135889c7c3bdc1e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13bfa223cd47326d4e50528a3cb8c4ae",
+    "url": "/%7BMiniprojekt2%7D/index.html"
+  },
+  {
+    "revision": "a6cc6eff8d6f9966bf2b",
+    "url": "/%7BMiniprojekt2%7D/static/css/main.6fa5af26.chunk.css"
+  },
+  {
+    "revision": "07a9608638f5f270c850",
+    "url": "/%7BMiniprojekt2%7D/static/js/2.50ee4acd.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/%7BMiniprojekt2%7D/static/js/2.50ee4acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6cc6eff8d6f9966bf2b",
+    "url": "/%7BMiniprojekt2%7D/static/js/main.1c48cc56.chunk.js"
+  },
+  {
+    "revision": "6dc9e977b60bd2f32558",
+    "url": "/%7BMiniprojekt2%7D/static/js/runtime-main.f23df1cf.js"
+  },
+  {
+    "revision": "4f4cd454718994fef116d50c861bc1f4",
+    "url": "/%7BMiniprojekt2%7D/static/media/Omoss1.4f4cd454.jpg"
+  },
+  {
+    "revision": "7e69cede1a6ac2412d322366fcc20450",
+    "url": "/%7BMiniprojekt2%7D/static/media/Omoss2.7e69cede.jpg"
+  },
+  {
+    "revision": "23ea9f8da5f3a84bb9c851dc0c0cbd1b",
+    "url": "/%7BMiniprojekt2%7D/static/media/Omoss3.23ea9f8d.jpg"
+  },
+  {
+    "revision": "7e2e10231bc5b01d3b9a905fe5d8f05e",
+    "url": "/%7BMiniprojekt2%7D/static/media/ceramics1.7e2e1023.jpg"
+  },
+  {
+    "revision": "724c35e8fdc78402747e68d480d475f7",
+    "url": "/%7BMiniprojekt2%7D/static/media/ceramics2.724c35e8.jpg"
+  },
+  {
+    "revision": "6166e928b7fca8e885efbd9f688ee182",
+    "url": "/%7BMiniprojekt2%7D/static/media/ceramics3.6166e928.jpg"
+  },
+  {
+    "revision": "ff0e63b9deed330ea79c192d83e531a4",
+    "url": "/%7BMiniprojekt2%7D/static/media/ceramics4.ff0e63b9.jpg"
+  }
+]);
